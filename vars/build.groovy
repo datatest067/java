f4b0847bfaa26403e583da28ed4ebe8312fd8fb4
datatest067 <80053914+datatest067@users.noreply.github.com>
2022-07-17 22:21:38 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -10,7 +10,7 @@ def run(dockerImageName)
 {
     sh "docker run -t --name javamaven -d ${dockerImageName}"
     sh "docker ps"
-    sh "docker cp ${containerName}:/usr/local/tomcat/webapps/wizard.war /tmp"
+    sh "docker cp javamaven:/usr/local/tomcat/webapps/wizard.war /tmp"
 }
 
 def cp()
